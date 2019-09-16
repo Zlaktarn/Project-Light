952a8d9d26a6c580708fd6c215ef7bb93f8d3d88
Zlaktarn <37144763+Zlaktarn@users.noreply.github.com>
2019-09-16 11:43:01 +0200

    Added prefabs
    Made a prefab for Player, Loot and Platform

--- Project Light/Assets/Scripts/PlayerScript.cs
+++ Project Light/Assets/Scripts/PlayerScript.cs
@@ -8,33 +8,15 @@ public class PlayerScript : MonoBehaviour
     [SerializeField] private string verticalInputName;
     [SerializeField] private float movementSpeed;
 
-
-    int forwardSpeed;
-    int sideSpeed;
-
     private CharacterController charController;
-
-    Rigidbody player;
 
     private void Awake()
     {
         charController = GetComponent<CharacterController>();
     }
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        player = GetComponent<Rigidbody>();
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        //transform.Translate(Vector3.forward * forwardSpeed * Time.deltaTime);
-        //transform.Translate(Vector3.right * sideSpeed * Time.deltaTime);
-
         Controls();
     }
 
@@ -47,21 +29,5 @@ public class PlayerScript : MonoBehaviour
         Vector3 rightMovement = transform.right * hInput;
 
         charController.SimpleMove(forwardMovement + rightMovement);
-
-        //if (Input.GetKey(KeyCode.W))
-        //    forwardSpeed = 3;
-        //else if (Input.GetKey(KeyCode.S))
-        //    forwardSpeed = -2;
-        //else
-        //    forwardSpeed = 0;
-
-        //if (Input.GetKey(KeyCode.A))
-        //    sideSpeed = -2;
-        //else if (Input.GetKey(KeyCode.D))
-        //    sideSpeed = 2;
-        //else
-        //    sideSpeed = 0;
     }
-
-    
 }
