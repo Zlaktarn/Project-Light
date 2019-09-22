a19317622b16973ba1b3c4e4b9ea5a6abea416ec
laila <lailalaila332@hotmail.com>
2019-09-22 23:25:00 +0200

    Inv+FP+testObj

---
+++ Project Light/Assets/LailasScripts/CameraMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    Vector2 mouseLook, smooth;
+    public float sensitivity = 5.0f;
+    public float smoothness = 2.0f;
+
+    GameObject character;
+
+    void Start()
+    {
+        character = this.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mousedir = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        mousedir = Vector2.Scale(mousedir, new Vector2(sensitivity * smoothness, sensitivity * smoothness));
+        smooth.x = Mathf.Lerp(smooth.x, mousedir.x, 1f / smoothness);
+        smooth.y = Mathf.Lerp(smooth.y, mousedir.y, 1f / smoothness);
+
+        mouseLook += smooth;
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+    }
+}
---
+++ Project Light/Assets/LailasScripts/InventorySlot.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+    public Image icon;
+    public Button removeButton;
+
+    Item item;
+
+    public void AddItem(Item newItem)
+    {
+        item = newItem;
+
+        icon.sprite = item.icon;
+        icon.enabled = true;
+        removeButton.interactable = true;
+    }
+
+    public void ClearSlot()
+    {
+        item = null;
+        icon.sprite = null;
+        icon.enabled = false;
+        removeButton.interactable = false;
+    }
+
+    public void RemoveButton()
+    {
+        Inventory.instance.Remove(item);
+    }
+
+}
