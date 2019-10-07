948a22a2fdec1f93338ca45ecf2105ffa97bd465
Eilorw <gr.oliwer@gmail.com>
2019-10-07 18:11:46 +0200

    Oliwer lägger till sina ljud scripts

---
+++ Project Light/Assets/Scripts/DetectionScripts/GravelDetection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravelDetection : MonoBehaviour
+{
+    private SoundTracker soundTracker;
+
+    public static int thisType = 2;
+
+    void Start()
+    {
+        
+    }
+
+    public void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            soundTracker = GameObject.Find("AudioObject").GetComponent<SoundTracker>();
+            soundTracker.floorTypes = thisType;
+            print("setting gravel");
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Project Light/Assets/Scripts/EnemyAudio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAudio : MonoBehaviour
+{
+    public AudioClip ambience;
+
+    public AudioSource SoundSource;
+
+    void Start()
+    {
+        SoundSource.clip = ambience;
+        SoundSource.loop = true;
+        SoundSource.spatialBlend = 1;
+        SoundSource.Play();
+    }
+    
+    void Update()
+    {
+        
+    }
+}
