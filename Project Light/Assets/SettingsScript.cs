91c99bdb3adb13105164be627b02ff965f932f63
Eilorw <gr.oliwer@gmail.com>
2019-11-27 18:01:37 +0100

    Settings Functionality Finished

--- Project Light/Assets/SettingsScript.cs
+++ Project Light/Assets/SettingsScript.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsScript : MonoBehaviour
 {
@@ -10,9 +12,43 @@ public class SettingsScript : MonoBehaviour
     public GameObject settingsMenuUI;
     public GameObject helpMenu;
 
+    public Dropdown resolutionDropdown;
+
+    public AudioMixer audioMixer;
+
     public bool settingsShow;
     public bool helpShow;
-    
+
+    Resolution[] resolutions;
+    QualitySettings[] qualities;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.P) && !settingsShow && !helpShow)
@@ -34,12 +70,15 @@ public class SettingsScript : MonoBehaviour
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
     }
 
     void Pause()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
         GameIsPaused = true;
     }
 
@@ -79,5 +118,20 @@ public class SettingsScript : MonoBehaviour
     public void QuitGame()
     {
         Debug.Log("Quitting Game...");
+    }
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
