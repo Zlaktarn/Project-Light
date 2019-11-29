a260e54f3c5750142013b0e86c4567a7a5ebd12a
Oscar <ogge2k@hotmail.com>
2019-11-29 14:58:33 +0100

    LoadingScene

---
+++ Project Light/Assets/LoadScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScene : MonoBehaviour
+{
+    [SerializeField]
+    private Image progressBar;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadAsyncOperation());   
+    }
+
+    IEnumerator LoadAsyncOperation()
+    {
+        AsyncOperation gameLevel = SceneManager.LoadSceneAsync(2);
+
+        while(gameLevel.progress < 0)
+        {
+            progressBar.fillAmount = gameLevel.progress;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+}
--- Project Light/Assets/Scripts/MenuScripts/MainMenu.cs
+++ Project Light/Assets/Scripts/MenuScripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("FINALSCENE");
+        SceneManager.LoadScene("LoadingScreen");
     }
 
     public void QuitGame()
