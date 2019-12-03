65f62678f784d51164686df7dd3061b54d21c21e
Oscar <ogge2k@hotmail.com>
2019-12-03 16:18:27 +0100

    ParticleSystem around player

---
+++ Project Light/Assets/Scripts/PlayerScripts/FixRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class FixRotation : MonoBehaviour
+{
+
+    Quaternion rotation;
+
+    void Awake()
+    {
+        rotation = transform.rotation;
+    }
+
+    void LateUpdate()
+    {
+        transform.rotation = rotation;
+    }
+}
