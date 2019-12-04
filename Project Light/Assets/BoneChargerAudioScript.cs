ea94c64b7b5e201616b6267122743edac657d630
Eilorw <gr.oliwer@gmail.com>
2019-12-04 18:15:22 +0100

    audio updates

---
+++ Project Light/Assets/BoneChargerAudioScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneChargerAudioScript : MonoBehaviour
+{
+    public AudioClip mediumBoiWalk;
+    public AudioClip mediumBoiCharge;
+
+    public AudioSource mediumBoiWalkAudio;
+    public AudioSource mediumBoiChargeAudio;
+
+    public bool charging;
+
+    public bool hostile = false;
+
+    private GameObject BoneCharger;
+    private AIChargeAttack BoneScript;
+
+    void Start()
+    {
+        mediumBoiWalkAudio.clip = mediumBoiWalk;
+        mediumBoiChargeAudio.clip = mediumBoiCharge;
+        mediumBoiChargeAudio.loop = true;
+        mediumBoiWalkAudio.loop = true;
+
+        BoneCharger = GameObject.FindWithTag("BoneCharger");
+        BoneScript = BoneCharger.GetComponent<AIChargeAttack>();
+    }
+    
+    void Update()
+    {
+        charging = BoneScript.attacking;
+
+        if(charging && hostile)
+        {
+            mediumBoiChargeAudio.Play();
+            mediumBoiWalkAudio.Stop();
+            hostile = false;
+
+        }
+        else if(!charging && !hostile)
+        {
+            mediumBoiWalkAudio.Play();
+            mediumBoiChargeAudio.Stop();
+            hostile = true;
+        }
+        else
+        {
+            Debug.Log("Something went wrong here!");
+        }
+        
+
+    }
+}
